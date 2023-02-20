54a0849e13c4b90b72cae4ddc4f41c28e992c697
Morumbi <zun0323@gmail.com>
2023-02-20 14:05:24 -0300

    add useParams

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,16 @@
 import { useParams } from 'react-router-dom';
 
+interface RouteParams {
+  coinId: string;
+}
+
 function Coin() {
-  const params = useParams();
-  console.log(params);
-  return <h1>Coin</h1>;
+  const { coinId } = useParams<RouteParams>();
+  return (
+    <div>
+      <h1>Coin : {coinId}</h1>
+    </div>
+  );
 }
 
 export default Coin;
