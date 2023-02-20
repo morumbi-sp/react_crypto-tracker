9512ef5f785c57be20b19d225556f6cee9d53b09
Morumbi <zun0323@gmail.com>
2023-02-20 13:44:44 -0300

    add home & id router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,19 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Coin from './routes/Coin';
+import Coins from './routes/Coins';
 
-function Router() {}
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/:coinId'>
+          <Coin />
+        </Route>
+        <Route path='/'>
+          <Coins />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
 export default Router;
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,8 @@
+import { useParams } from 'react-router-dom';
+
 function Coin() {
+  const params = useParams();
+  console.log(params);
   return <h1>Coin</h1>;
 }
 
