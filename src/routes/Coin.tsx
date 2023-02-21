cd6fb1710cf08375b0109eea61ebe5057da75bec
Morumbi <zun0323@gmail.com>
2023-02-21 14:27:15 -0300

    add interface quotes

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -33,6 +33,26 @@ interface InfoData {
   last_data_at: string;
 }
 
+interface Quotes {
+  price: number;
+  volume_24h: number;
+  volume_24h_change_24h: number;
+  market_cap: number;
+  market_cap_change_24h: number;
+  percent_change_15m: number;
+  percent_change_30m: number;
+  percent_change_1h: number;
+  percent_change_6h: number;
+  percent_change_12h: number;
+  percent_change_24h: number;
+  percent_change_7d: number;
+  percent_change_30d: number;
+  percent_change_1y: number;
+  ath_price: number;
+  ath_date: string;
+  percent_from_price_ath: number;
+}
+
 interface PriceData {
   id: string;
   name: string;
@@ -44,6 +64,9 @@ interface PriceData {
   beta_value: number;
   first_data_at: string;
   last_updated: string;
+  quotes: {
+    USD: Quotes;
+  };
 }
 
 const Container = styled.div`
